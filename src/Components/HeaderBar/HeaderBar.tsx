35f91912b69baa3fcedde780462e21aa70cc40c0
Deividas Ovsianikovas <deividas.ovsianikovas.2020@mumail.ie>
2023-03-03 16:13:13 +0000

    Tests header bar

--- src/Components/HeaderBar/HeaderBar.tsx
+++ src/Components/HeaderBar/HeaderBar.tsx
@@ -1,17 +1,13 @@
 import { Typography } from "@mui/material"
 
 const HeaderBar = () => {
-    /// Return a header that spans the whole page and is 100 height
     return (
-        <div className="header-bar" style={{ backgroundColor: 'white' }}>
-            <div className="header-bar__title">
+        <div style={{ backgroundColor: 'white' }}>
+            <div>
                 <Typography variant="h5">KG Workforce Forecaster Scheduler</Typography>
             </div>
         </div>
     )
-
 }
-
-
 
 export { HeaderBar }
