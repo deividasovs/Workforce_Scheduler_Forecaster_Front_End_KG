62dba37c0a7fd8aaa7366fc778e69a1d64d5e983
Deividas Ovsianikovas <deividas.ovsianikovas.2020@mumail.ie>
2023-03-13 20:44:08 +0000

    Tests Historical Rotas page

--- src/Pages/HistoricalRotasPage/HistoricalRotasPage.spec.tsx
+++ src/Pages/HistoricalRotasPage/HistoricalRotasPage.spec.tsx
@@ -1,11 +1,16 @@
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from '@testing-library/react';
+import { HistoricalRotasPage } from './HistoricalRotasPage';
 
-import { HistoricalRotasPage } from "./HistoricalRotasPage";
+describe('HistoricalRotasPage', () => {
+    it('should change the displayed rota when a different option is selected from the dropdown', () => {
+        render(<HistoricalRotasPage />);
 
-describe('historical rotas page', () => {
-    it('renders', () => {
-        const { getByText } = render(<HistoricalRotasPage />);
-        const headerTitle = getByText('hello');
-        expect(headerTitle).toBeInTheDocument();
+        // Select the second option from the dropdown
+        const dropdown = screen.getByLabelText('Rota');
+        fireEvent.change(dropdown, { target: { value: '2' } });
+
+        // Check that the displayed rota has changed
+        const rotaViewer = screen.getByRole('tabpanel', { name: 'Department 1' });
+        expect(rotaViewer).toHaveTextContent('Sample Rota 2');
     });
 });
